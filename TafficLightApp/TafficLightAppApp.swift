82f12d755548998a92811b38a7d6fcc2e0c3e6ca
Diana Ovechkina <ovechkinadiana@yandex.ru>
2022-02-15 21:21:01 +0300

    Initial Commit

---
+++ TafficLightApp/TafficLightAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TafficLightAppApp.swift
+//  TafficLightApp
+//
+//  Created by Diana Ovechkina on 15.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct TafficLightAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
